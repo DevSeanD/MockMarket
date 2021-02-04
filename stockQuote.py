cda14b2ec614a8266f91c8e98604ebef798c4408
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2021-02-04 10:42:16 -0500

    Init stockQuote.py

---
+++ stockQuote.py
@@ -0,0 +1,17 @@
+"""
+Author: Sean Dever
+Description: The purpose of this file is to serve as a starting point for the overall mock stock exchange project. It will ask the user for a stock and using the yahoo finance api it will rectrieve the current price.
+File Name: stockQuote.py
+
+Dependancies: 
+  pip install requests_html
+  pip install yfinance
+  pip install yahoo_fin
+"""
+
+import yfinance
+import yahoo_fin.stock_info as si
+
+targetStock = input("What stock would you like to quote? ")
+
+print(si.get_data(targetStock))

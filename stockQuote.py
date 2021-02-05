5f65757d06cf616f45b717458f55233d53969426
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2021-02-05 09:04:43 -0500

    Multiple stocks may now be quoted
    A while loop was added so the user may request a quote for multiple stocks

--- stockQuote.py
+++ stockQuote.py
@@ -2,7 +2,6 @@
 Author: Sean Dever
 Description: The purpose of this file is to serve as a starting point for the overall mock stock exchange project. It will ask the user for a stock and using the yahoo finance api it will rectrieve the current price.
 File Name: stockQuote.py
-
 Dependancies: 
   pip install requests_html
   pip install yfinance
@@ -12,7 +11,9 @@ Dependancies:
 import yfinance
 import yahoo_fin.stock_info as si
 
-targetStock = input("What stock would you like to quote? ")
 
-print(si.get_live_price(targetStock))
+while(True):
+    targetStock = input("What stock would you like to quote? ")
 
+    print(si.get_live_price(targetStock))
+    print("")

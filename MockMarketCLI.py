7cb5a0bfe6e7858cf4b97b32f120f83ac110babf
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2021-09-23 14:13:20 -0400

    Added portfolioSummary Function
    portfolioSummary function - prints to the console the stock names, stock price bought at, and quantities of stocks owned

--- MockMarketCLI.py
+++ MockMarketCLI.py
@@ -11,35 +11,55 @@ Dependancies:
 import json
 import yahoo_fin.stock_info as si
 
+def mainMenu():
+  print("=========")
+  print("Main Menu")
+  print("=========")
+  print()
+  print()
+  print("1 - Poftfolio Summary")
+  print("2 -")
+
+def portfolioSummary():
+  with open('portfolio.json') as file:
+    portfolio = json.load(file)
+
+  print("Stock Name",'\t',"Bought At",'\t','\t','    ',"Number of Shares")
+  for index in range(len(portfolio['stockNames'])):
+    print(portfolio['stockNames'][index] , '\t','\t', portfolio['stockPrices'][index]," ",portfolio['shareQuantities'][index])
 
 def main():
-  portfolio = {}
   
   while (True):
     print()
     targetStock = input("What stock would you like to quote? ")
     
-    currPrice = si.get_live_price(targetStock)
+    try:
+      currPrice = si.get_live_price(targetStock)
+    except:
+      print("a Non Valid Ticker has been entered")
+      main()
+      
     print("Current price of",targetStock + ":",currPrice)
     print("")
 
     print("Would you like to buy shares of" , targetStock + "?")
-    buyOrNot = input("If yes enter Y followed by the number of shares:  ")
+    buyOrNot = input("If yes enter the number of shares:  ")
     
     if buyOrNot[0] == 'y' or buyOrNot =='Y':
       numOfShares = int(buyOrNot[1:])
 
-    with open('portfolio.json') as file:
-      portfolio = json.load(file)
+      with open('portfolio.json') as file:
+        portfolio = json.load(file)
       
-    portfolio['stockNames'].append(targetStock)
+      portfolio['stockNames'].append(targetStock)
 
-    portfolio['stockPrices'].append(si.get_live_price(targetStock))
+      portfolio['stockPrices'].append(si.get_live_price(targetStock))
     
-    portfolio['shareQuantities'].append(numOfShares)
+      portfolio['shareQuantities'].append(numOfShares)
 
-    with open('portfolio.json', 'w') as file:
-      json.dump(portfolio, file, indent=4)
+      with open('portfolio.json', 'w') as file:
+        json.dump(portfolio, file, indent=4)
 
 if __name__ == "__main__":
 	main()

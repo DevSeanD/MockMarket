758d633c0a6376751706a4246cf40a435e1c8b15
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2021-09-23 12:54:23 -0400

    Added More Json Support
    The json file now support the stockPrice and shareQuantities fields

--- MockMarketCLI.py
+++ MockMarketCLI.py
@@ -13,23 +13,33 @@ import yahoo_fin.stock_info as si
 
 
 def main():
+  portfolio = {}
+  
+  while (True):
+    print()
+    targetStock = input("What stock would you like to quote? ")
+    
+    currPrice = si.get_live_price(targetStock)
+    print("Current price of",targetStock + ":",currPrice)
+    print("")
 
-	portfolio = {}
+    print("Would you like to buy shares of" , targetStock + "?")
+    buyOrNot = input("If yes enter Y followed by the number of shares:  ")
+    
+    if buyOrNot[0] == 'y' or buyOrNot =='Y':
+      numOfShares = int(buyOrNot[1:])
 
-	while (True):
-		targetStock = input("What stock would you like to quote? ")
+    with open('portfolio.json') as file:
+      portfolio = json.load(file)
+      
+    portfolio['stockNames'].append(targetStock)
 
-		print(si.get_live_price(targetStock))
-		print("")
+    portfolio['stockPrices'].append(si.get_live_price(targetStock))
+    
+    portfolio['shareQuantities'].append(numOfShares)
 
-		with open('portfolio.json') as file:
-			portfolio = json.load(file)
-
-		portfolio['stock_names'].append(targetStock)
-
-		with open('portfolio.json', 'w') as file:
-			json.dump(portfolio, file, indent=4)
-
+    with open('portfolio.json', 'w') as file:
+      json.dump(portfolio, file, indent=4)
 
 if __name__ == "__main__":
 	main()

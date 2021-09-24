b458aebb1e66564bceb5574f3c94451312bbe02e
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2021-09-24 09:29:28 -0400

    Added TODO Section

--- MockMarketCLI.py
+++ MockMarketCLI.py
@@ -2,11 +2,16 @@
 Author: Sean Dever
 Description: The purpose of this file is to serve as a starting point for the overall mock stock market project. It will ask the user for a stock and using the yahoo finance api it will rectrieve the current price.
 File Name: MockMarketCLI.py
-Dependancies: 
-  pip install requests_html
+Dependancies:
   pip install yfinance
   pip install yahoo_fin
+
+TODO:
+  BuyShares function
+  SellShares function
+  Round stock price values
 """
+
 import json
 import yahoo_fin.stock_info as si
 
@@ -106,4 +111,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-:
